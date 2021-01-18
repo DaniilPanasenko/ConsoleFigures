0ec85901932e941c7ff795c22e2fcb770b78e783
DaniilPanasenko <daniil.panasenko@nure.ua>
2021-01-18 16:28:08 +0200

    add classes

---
+++ СonsoleFigures/Classes/Figure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using СonsoleFigures.Interfaces;
+
+namespace СonsoleFigures.Classes
+{
+    abstract public class Figure : IFigure
+    {
+        public string Name { get; set; }
+
+        public abstract decimal Square { get; }
+
+        public abstract decimal Perimetr { get; }
+
+        public abstract Point Coordinates { get; }
+
+        public Figure(string name)
+        {
+            Name = name;
+        }
+
+        public abstract int[,] ToMatrix();
+
+        public abstract override string ToString();
+
+        public string ToDimensionSring()
+        {
+            return $"Square = {Square} Perimetr = {Perimetr}";
+        }
+    }
+}
---
+++ СonsoleFigures/Classes/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace СonsoleFigures.Classes
+{
+    public class Rectangle : Figure
+    {
+        public Point LeftTopPoint { get; private set; }
+
+        public decimal Width { get; private set; }
+
+        public decimal Height { get; private set; }
+
+        public override Point Coordinates => LeftTopPoint;
+
+        public override decimal Square => Width * Height;
+
+        public override decimal Perimetr => 2 * Width + 2 * Height;
+
+        public Rectangle(Point point, decimal width, decimal height, string name = "Rectangle") : base(name)
+        {
+            LeftTopPoint = point;
+            Width = width;
+            Height = height;
+        }
+
+        public override int[,] ToMatrix()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: Coodinates = {LeftTopPoint} Width = {Width} Height = {Height} {ToDimensionSring()}";
+        }
+    }
+}
